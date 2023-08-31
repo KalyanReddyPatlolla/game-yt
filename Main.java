b191ba979ed957182c84658df638333fd41f25e0
Venkata Kalyan Reddy Patlolla <102273194+KalyanReddyPatlolla@users.noreply.github.com>
2023-08-31 22:45:04 +0530

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,12 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+import javax.swing.*;
+import java.util.*;
+public class Main {
+
+    public static void main(String[] args) {
+
+        TicTacToe tictactoe = new TicTacToe();
+
+    }
+}
